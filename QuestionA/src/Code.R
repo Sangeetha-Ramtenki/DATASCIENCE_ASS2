3c0bae77c05fd875495a278c971ae6e508091bbf
Achyuth9 <107339131+Achyuth9@users.noreply.github.com>
2023-04-13 06:36:16 -0500

    Update Code.R

--- QuestionA/src/Code.R
+++ QuestionA/src/Code.R
@@ -1,14 +1,18 @@
 
 #To ensure work reproducibility
-
 set.seed(7)
+
 diabetes<- read.csv('~/Downloads/diabetes.csv')
+
 sample_data <- diabetes[sample(nrow(diabetes), 25), ]
+
 View(sample_data)
+
 mean_value_sample <- mean(sample_data$Glucose)
 highest_value_sample <- max(sample_data$Glucose)
 mean_value_diabetes <- mean(diabetes$Glucose)
 highest_value_diabetes <- max(diabetes$Glucose)
+
 
 df <- data.frame(Max_Mean=rep(c('Mean', 'Max'), each=2),
                  position=rep(c('Sample', 'Diabetes'), times=2),
@@ -19,6 +23,7 @@ df
 
 library(ggplot2)
 
+#plotting the chart
 ggplot(df, aes(fill=position, y=Glucose_Values, x=Max_Mean)) + 
   geom_bar(position='dodge', stat='identity')
 
